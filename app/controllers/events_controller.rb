fd4e895d4bd0147adc32414004cf2814e8dc2123
yar <yar.ua.1985@gmail.com>
2017-01-17 00:24:52 +0200

    add create event view
    don'n work :adress field, not recieve inputted data's

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,13 +3,37 @@ class EventsController < ApplicationController
   # проверить залогинен ли юзер, исключение - просмотр существующих евентов
   before_action :authenticate_user!, except: [:show, :index]
 
+  
+  # GET /events
   def index
   	@event = Event.all
   end
 
+
+  # GET /events/1
   def new
+  	@event = current_user.events.build
   end
 
+  # POST /evebts
   def create
+  	@event = current_user.events.build(event_params)
+
+  	if @event.save
+  	  redirect_to @event, notice: 'Событие успешно создано'
+  	else
+  	  render :new
+  	end
   end
+
+
+
+
+  protected
+
+  def event_params
+  	params.require(:event).permit(:title, :adress, :datetime, :description) # , :pincode)
+  end
+
+
 end
