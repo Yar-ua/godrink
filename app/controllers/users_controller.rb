0ba1413091f72bcf5d2344d1ca18e780b8566505
yar <yar.ua.1985@gmail.com>
2017-01-16 23:21:27 +0200

    added editing profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,26 @@ class UsersController < ApplicationController
   end
 
 
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Профиль обновлен'
+    else
+      render 'edit'
+    end
+  end
+
+
   private
 
   def set_current_user
   	@user = current_user
+  end
+
+
+  # пользователь в этом контроллере может менять имя, емейл
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
