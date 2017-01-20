d4cd86ece272fa2b0abce7ed656ec0e949ef6ad7
yar <yar.ua.1985@gmail.com>
2017-01-20 22:08:31 +0200

    refactoring

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ApplicationRecord
   # событие принадлежит юзеру
   belongs_to :user
 
+  # событие имеет много комментариев и подписок
+  has_many :comments, dependent: :destroy
+
   # проверка, событие создает юзер. юзера не может не быть
   validates :user, presence: true
   # заголовок должен быть, и не длиннее 255 букв
