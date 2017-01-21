db73fc98870808956066e43b3e43ce962d4f581f
yar <yar.ua.1985@gmail.com>
2017-01-21 17:08:58 +0200

    update subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,5 +3,36 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
+  validates :event, presence: true
+
+  # валидации выполняются если user не задан (незарегистрированные юзеры)
+  validates :user_name, presence: true, unless: 'user.present?'
+  #validates :user_email, presence: true, 
+  #			format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: 'user.present?'
+
+  # для данного event_id один юзер может подписаться только один раз
+  validates :user, uniqueness: {scope: :event_id}, if: 'user.resent?'
+
+  # переопределяем метод, если есть юзер, выдаем его имя,
+  # если нет -- дергаем исходный переопределенный метод
+  def user_name
+  	if user.present?
+  	  user.name
+  	else
+  	  super
+  	end
+  end
+
+
+  # переопределяем метод, если есть юзер, выдаем его email,
+  # если нет -- дергаем исходный переопределенный метод
+  def user_email
+  	if user.present?
+  	  user.email
+  	else
+  	  super
+  	end
+  end
 
 end
+
