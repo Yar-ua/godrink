8c659833997adf9269b65d0951ef19b0598c1e89
yar <yar.ua.1985@gmail.com>
2017-01-29 20:54:42 +0200

    add create photo to event#show page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,4 +13,9 @@ class Photo < ApplicationRecord
 
   # Добавляем аплоадер фотографий, чтобы заработал carrierwave
   mount_uploader :photo, PhotoUploader
+
+  # этот scope нужен чтобы отделить реальные фото от болванной фотки
+  # см. events_controller
+  scope :persisted, -> { where 'id IS NOT NULL' }
+
 end
