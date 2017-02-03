1c892f7b990a2e6c126779954d8ea2c4276df0dd
yar <yar.ua.1985@gmail.com>
2017-02-03 21:36:55 +0200

    lightbox works
    script-code is in the event#show file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,15 +17,3 @@
 //= require_tree .
 
 
-
-// Скрываем форму добавления фотографии и показываем её по клику на ссылку
-// ++! требуется доработка
-/* $(function(){
-  $('.event-photo-form').hide();
-
-  $('.event-photo-form-link').click(function(){
-    $('.event-photo-form').slideToggle(300);
-    return false;
-  });
-});
-*/
