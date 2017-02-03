ffcbb3b3388e1c25e82a924d09028a3f0ff05bbf
yar <yar.ua.1985@gmail.com>
2017-02-03 18:32:17 +0200

    add random photo to title photo of event#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,5 +26,31 @@ module ApplicationHelper
     end
   end
 
+
+  # возвращает адрес случайной фотки события, если есть хотя бы одна фотка события
+  # иначе возвращает ссылку на случайную картинку
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('background_event.jpg')
+    end
+  end
+
+
+  # возвращает мини-версию фотографии из евента для отображения на обложке
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
+
+  end
+
 	
 end
