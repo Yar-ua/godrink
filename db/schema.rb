277544230d94d2f75487941736147b710af8cdd2
yar <yar.ua.1985@gmail.com>
2017-03-08 23:36:57 +0200

    inprovement DB - add timestamps to users
    drop table and create it again

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20170128071800) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
