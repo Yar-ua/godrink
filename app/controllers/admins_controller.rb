a4ad006ad97631b6edb7c7a71e94baf5e2ff4683
yar <yar.ua.1985@gmail.com>
2017-03-17 22:10:40 +0200

    add admin partial

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,11 +2,16 @@ class AdminsController < ApplicationController
   # назначаем другой слой со стилямиь для панели админа
   layout "admin_application"
   
-  before_action :authenticate_admin!, except: [:show]
+  before_action :authenticate_admin!
   
 
   def index
-  
+  	@admins = Admin.all
+  end
+
+
+  def new
+
   end
 
 
