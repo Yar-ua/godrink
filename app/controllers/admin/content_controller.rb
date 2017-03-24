927e7b08326e8b0eab3a5d133c7a1984a7ff56ba
yar <yar.ua.1985@gmail.com>
2017-03-24 23:48:11 +0200

    create admin:content:show method

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -10,6 +10,12 @@ class Admin::ContentController < ApplicationController
 
   end
 
+  def show
+  	@event = Event.find(params[:id])
+  	@comments_event_collection = @event.comments.paginate(page: params[:page], per_page: 10).order('created_at DESC')
+  	render 'events/show'
+  end
+
 
   def new
 
