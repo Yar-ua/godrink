4646edc591741ea88325ba9335559ff1ee186840
yar <yar.ua.1985@gmail.com>
2017-04-01 22:19:56 +0300

    I think, i fixed the bug with deletind images one week ago

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,7 +13,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   # папка для хранения файлов аватарок
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -17,7 +17,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Папка, в которой будут храниться все наши загруженные файлы
   # например, uploas/photo/photo/1
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
